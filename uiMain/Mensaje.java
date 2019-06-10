6a0efac333e6a65402088ad937800142db6f5d34
MauricioChavarria <47341821+MauricioChavarria@users.noreply.github.com>
2019-06-10 11:26:04 -0500

    Update Mensaje.java

--- uiMain/Mensaje.java
+++ uiMain/Mensaje.java
@@ -3,17 +3,4 @@ import java.util.HashMap;
 
 public class Mensaje {
 
-    public static HashMap<String, String> espanol = new HashMap<String, String>() {
-        {
-            put("Bienvenido", "2");
-        }
-
-    };
-
-    public static HashMap<String, String> ingles = new HashMap<String, String>() {
-        {
-            put("Welcome", "2");
-        }
-        
-    };
 }
