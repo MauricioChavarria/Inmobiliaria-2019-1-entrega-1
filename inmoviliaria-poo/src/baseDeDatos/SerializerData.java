4946b1456a16047967f82220a3b6de7142caa47b
Camilo Rivera <camilorivera108@gmail.com>
2019-07-25 20:17:05 -0500

    Serializer modificado

--- inmoviliaria-poo/src/baseDeDatos/SerializerData.java
+++ inmoviliaria-poo/src/baseDeDatos/SerializerData.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.*;
-import gestorAplicacion.*;
+
+import gestorAplicacion.paqueteServicios.*;
+import gestorAplicacion.paqueteUsuarios.*;
 
 
 public class SerializerData {
@@ -18,62 +20,43 @@ public class SerializerData {
 	static String nombreArchivo = System.getProperty("user.dir")+"/src/temp/dataSerialized";
 
 	
-	public SerializerData(){
-		Boolean emptyAdministrador = true;
-		Boolean emptyCliente = true;
-		Boolean emptyFuncionario = true;
-		Boolean emptyInmueble = true;
-		Boolean emptyArriendo = true;
-		Boolean emptyCompraventa = true;
-		Boolean emptyContrato = true;
-		
-
-
-		if(!Administrador.admins.isEmpty()){
-			objetos.push(Administrador.admins);
-		}else emptyAdministrador = false;
-
-		if(!Cliente.clientes.isEmpty()){
-			objetos.push(Cliente.clientes);
-		}else emptyFuncionario = false;
-
-		if(!Funcionario.funcionarios.isEmpty()){
-			objetos.push(Funcionario.funcionarios);
-		}else emptyFuncionario = false;
-		
-		if(!Inmueble.inmuebles.isEmpty()){
-			objetos.push(Inmueble.inmuebles);
-		}else emptyInmueble = false;
-		
-		if(!Arriendo.arriendos.isEmpty()){
-			objetos.push(Arriendo.arriendos);
-		}else emptyArriendo = false;
-		
-		if(!Compraventa.compraventas.isEmpty()){
-			objetos.push(Compraventa.compraventas);
-		}else emptyCompraventa = false;
-		
-		if(!Contrato.contratos.isEmpty()){
-			objetos.push(Contrato.contratos);
-		}else emptyContrato = false;
-		
-		
+	public SerializerData() {
+		try{
+			serializeDataIn();
+			Administrador.admins = (List<Administrador>) this.objetos.get(0);
+		}catch(ClassNotFoundException | IOException e){
+			System.out.println("No se puedo inicializar el programa");
+		}
 	}
 	
-	public static void serializeDataOut()throws IOException{
+	private static void serializeDataOut()throws IOException{
 	    FileOutputStream fos = new FileOutputStream(nombreArchivo);
 	    ObjectOutputStream oos = new ObjectOutputStream(fos);
 	    oos.writeObject(objetos);
 	    oos.close();
 	}
 
-	public static LinkedList serializeDataIn() throws IOException, ClassNotFoundException{
+	public static void serializeDataIn() throws IOException, ClassNotFoundException{
 
 	   FileInputStream fin = new FileInputStream(nombreArchivo);
 	   ObjectInputStream ois = new ObjectInputStream(fin);
-	   LinkedList<Object> objetos = (LinkedList)ois.readObject();
+	   objetos = (LinkedList)ois.readObject();
 	   ois.close();
-	   return objetos;
+	}
+
+	public void finalize() {
+		objetos.push(Administrador.admins);
+		objetos.push(Cliente.clientes);
+		objetos.push(Funcionario.funcionarios);
+		objetos.push(Inmueble.inmuebles);
+		objetos.push(Arriendo.arriendos);
+		objetos.push(Compraventa.compraventas);
+		objetos.push(Contrato.contratos);
+		try{
+			serializeDataOut();
+		}catch(IOException e){
+			System.out.println("Error al guardar datos");
+		}
 	}
 
 }
