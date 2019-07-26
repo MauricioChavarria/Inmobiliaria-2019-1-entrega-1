c7cf95b3de26f0a8db292be9533973735f4175be
MauricioChavarria <47341821+MauricioChavarria@users.noreply.github.com>
2019-07-26 03:01:59 -0500

    se cambiaron los metodos de administrador para que funcionen bien

--- inmoviliaria-poo/src/gestorAplicacion/paqueteUsuarios/Administrador.java
+++ inmoviliaria-poo/src/gestorAplicacion/paqueteUsuarios/Administrador.java
@@ -24,29 +24,23 @@ public class Administrador extends Usuario implements Serializable{
 		admins.add(this);
 	}
 	
-	public static LinkedList<Funcionario> listarFuncionarios(LinkedList<Funcionario> todos_funcionarios){
-		 LinkedList<Funcionario> funcionarioa_encontrados = new LinkedList<Funcionario>();
-	        for (Funcionario iterado : todos_funcionarios) {
-	        	funcionarioa_encontrados.add(iterado);
-	        }
-	        return funcionarioa_encontrados;
+	public static void listarFuncionarios(){
+		 for(int i=0; i<=Funcionario.funcionarios.size();i++) {
+			 System.out.println("("+Funcionario.funcionarios.get(i).getNombre()+", "+Funcionario.funcionarios.get(i).getCedula()+", "+Funcionario.funcionarios.get(i).getCorreo()+", "+Funcionario.funcionarios.get(i).getDireccion()+", "+Funcionario.funcionarios.get(i).getSueldo()+", "+Funcionario.funcionarios.get(i).getComision()+")");
+		 }
 	}
 	
-	public static LinkedList<Cliente> listarClientes(LinkedList<Cliente> todos_clientes) {
-        LinkedList<Cliente> clientes_encontrados = new LinkedList<Cliente>();
-        for (Cliente iterado : todos_clientes) {
-        	clientes_encontrados.add(iterado);
-        }
-        return clientes_encontrados;
-    }
+	public static void listarClientes() {
+		for(int i=0; i<=Cliente.clientes.size();i++) {
+			 System.out.println("("+Cliente.clientes.get(i).getNombre()+", "+Cliente.clientes.get(i).getCedula()+", "+Cliente.clientes.get(i).getCorreo()+", "+Cliente.clientes.get(i).getDireccion()+")");
+		 }
+   }
 	
-	public static LinkedList<Administrador> listarAdministradores(LinkedList<Administrador> todos_admins) {
-        LinkedList<Administrador> admins_encontrados = new LinkedList<Administrador>();
-        for (Administrador iterado : todos_admins) {
-        	admins_encontrados.add(iterado);
-        }
-        return admins_encontrados;
-    }
+	public static void listarAdministradores() {
+		for(int i=0; i<=Administrador.admins.size();i++) {
+			 System.out.println("("+Administrador.admins.get(i).getNombre()+", "+Administrador.admins.get(i).getCedula()+", "+Administrador.admins.get(i).getCorreo()+", "+Administrador.admins.get(i).getDireccion()+")");
+		 }
+   }
 	
 	public static LinkedList<Inmueble> listarInmuebles(LinkedList<Inmueble> todos_Inmuebles) {
         LinkedList<Inmueble> inmuebles_encontrados = new LinkedList<Inmueble>();
